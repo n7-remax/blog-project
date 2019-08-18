e9c35ff7a3816bfd6adc20320cde487856e1581f
N7_Remax <ditritusa@gmail.com>
2019-08-18 21:20:01 +0300

    Updates

---
+++ precache-manifest.d3ca7e8828efa12d03f289df95efac73.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2623f958359acca07bdd29d250d41b1b",
+    "url": "/blog-project/index.html"
+  },
+  {
+    "revision": "8e57a2335e585f4a9343",
+    "url": "/blog-project/static/css/main.917f2d38.chunk.css"
+  },
+  {
+    "revision": "815a04cec032f51399f0",
+    "url": "/blog-project/static/js/2.e7c989bb.chunk.js"
+  },
+  {
+    "revision": "8e57a2335e585f4a9343",
+    "url": "/blog-project/static/js/main.9b2e049f.chunk.js"
+  },
+  {
+    "revision": "f03cca0d2c6cd6dcb097",
+    "url": "/blog-project/static/js/runtime~main.53e32354.js"
+  }
+]);
